f1ab5b46ee538fc34cdbfc26658ffddb19015963
MiguelTrinca <miguel.trinca@tecnico.ulisboa.pt>
2023-10-25 19:44:32 +0100

    Added DistDir

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'export',
+  //output: 'export',
+  distDir: 'dist',
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
