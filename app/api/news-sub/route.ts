43691b7ab2b921b6ca61d78c8137bd5299d89472
MiguelTrinca <miguel.trinca@tecnico.ulisboa.pt>
2023-10-31 12:02:35 +0000

    Trying to fix reading files

--- app/api/news-sub/route.ts
+++ app/api/news-sub/route.ts
@@ -49,8 +49,9 @@ export async function POST(request: NextRequest){
                 //Send Email
                 try {
                     //Get Email in HTML form
-                    const emailPath = path.join(__dirname, '../../../../../public/newsletter/bem-vindo-padelit.html');
-                    const emailHtml = fs.readFileSync(emailPath, 'utf-8');
+                    const emailV2 = path.resolve("./public/newsletter/bem-vindo-padelit.html");
+                    //const emailPath = path.join(__dirname, '../../../../../public/newsletter/bem-vindo-padelit.html');
+                    const emailHtml = fs.readFileSync(emailV2, 'utf-8');
 
                     const transport = nodemailer.createTransport({
                         service: 'gmail',
